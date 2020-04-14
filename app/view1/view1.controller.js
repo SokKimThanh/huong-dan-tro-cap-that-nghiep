6745b78d28fdbdf53b8c2f57193531057770431f
Sok Kim Thanh <thanhsk1991@gmail.com>
2020-04-14 17:39:59 +0700

    update animation box-shadow image

--- app/view1/view1.controller.js
+++ app/view1/view1.controller.js
@@ -1,3 +1,4 @@
+'use strict';
 function View1Ctrl($scope) {
     $scope.disWork = [
         {
@@ -6,7 +7,7 @@ function View1Ctrl($scope) {
             "src": "setting/hoi-ve-dk-tro-cap-that-nghiep/dieu-kien-de-duoc-huong-tro-cap-that-nghiep-18621.jpg",
             "alt": "dieu-kien-de-duoc-huong-tro-cap-that-nghiep-18621",
             "href": '#!/hoi-ve-dieu-kien-tro-cap-that-nghiep',
-            "imgClass": "base-class float-left img-fluid"
+            "imgClass": "base-class float-left img-fluid imgShadow"
         },
         {
             "title": "Hỏi về quyết toán bảo hiểm xã hội",
@@ -14,7 +15,7 @@ function View1Ctrl($scope) {
             "src": "setting/hoi-ve-quyet-toan-bao-hiem-xa-hoi/noi-dang-ky-nhan-bao-hiem-that-nghiep-o-ho-chi-minh-va-ha-noi.png",
             "alt": "noi-dang-ky-nhan-bao-hiem-that-nghiep-o-ho-chi-minh-va-ha-noi",
             "href": '#!/hoi-ve-quyet-toan-bao-hiem-xa-hoi',
-            "imgClass": "base-class float-right img-fluid"
+            "imgClass": "base-class float-right img-fluid imgShadow"
         },
     ]
 }
