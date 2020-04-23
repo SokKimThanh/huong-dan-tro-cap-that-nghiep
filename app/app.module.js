cf01f398846867a310d0d5f694ba94b5a786efb1
Sok Kim Thanh <thanhsk1991@gmail.com>
2020-04-23 09:50:40 +0700

    update animation ng-view

--- app/app.module.js
+++ app/app.module.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
     'ngRoute',
+    'ngAnimate',
     'angularCSS',
     'myApp.view1',
     'myApp.view2',
