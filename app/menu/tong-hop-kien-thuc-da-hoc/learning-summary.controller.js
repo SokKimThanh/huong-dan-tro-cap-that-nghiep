8194d5b19dd9937cd1c8d88bad08baf19beb09a8
Sok Kim Thanh <thanhsk1991@gmail.com>
2020-05-02 20:04:27 +0700

    update tổng hợp kiến thức đã học - css name_variation

--- app/menu/tong-hop-kien-thuc-da-hoc/learning-summary.controller.js
+++ app/menu/tong-hop-kien-thuc-da-hoc/learning-summary.controller.js
@@ -15,6 +15,22 @@ function LearningSummaryCtrl($scope, $rootScope, LearningSummaryService) {
     $scope.answer5 = "";
     $scope.answer6 = "";
     $scope.answer7 = "";
+    $scope.name_variations_list = [
+        {
+            id_variation: "answer1", name_variation: "Number of characters", result_variation: $scope.answer1
+        }, {
+            id_variation: "answer2", name_variation: "First character", result_variation: $scope.answer2
+        }, {
+            id_variation: "answer3", name_variation: "Last character", result_variation: $scope.answer3
+        }, {
+            id_variation: "answer4", name_variation: "Lower case", result_variation: $scope.answer4
+        }, {
+            id_variation: "answer5", name_variation: "Upper case", result_variation: $scope.answer5
+        }, {
+            id_variation: "answer6", name_variation: "Capitalize", result_variation: $scope.answer6
+        }, {
+            id_variation: "answer7", name_variation: "Without leading \n and trailing spaces", result_variation: $scope.answer7
+        },];
     $scope.addEventListenerKeyup = function () {
         const value = $scope.ngModelYourName;
         $scope.answer1 = lss.getNumberOfChars(value);
@@ -28,5 +44,29 @@ function LearningSummaryCtrl($scope, $rootScope, LearningSummaryService) {
             $scope.answer6 = "";
         }
         $scope.answer7 = lss.getClean(value);
+        for (var x in $scope.name_variations_list) {
+            let variation = $scope.name_variations_list[x];
+            if (variation.id_variation == "answer1") {
+                variation.result_variation = $scope.answer1;
+            }
+            if (variation.id_variation == "answer2") {
+                variation.result_variation = $scope.answer2;
+            }
+            if (variation.id_variation == "answer3") {
+                variation.result_variation = $scope.answer3;
+            }
+            if (variation.id_variation == "answer4") {
+                variation.result_variation = $scope.answer4;
+            }
+            if (variation.id_variation == "answer5") {
+                variation.result_variation = $scope.answer5;
+            }
+            if (variation.id_variation == "answer6") {
+                variation.result_variation = $scope.answer6;
+            }
+            if (variation.id_variation == "answer7") {
+                variation.result_variation = $scope.answer7;
+            }
+        }
     }
 }
